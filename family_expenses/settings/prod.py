c661042550573bb5e8585a33cde271f4dd59f4ee
Claudio Cuellar <ccuellar@wine.com>
2025-09-07 17:06:29 -0400

    Enabling cors on prod

--- family_expenses/settings/prod.py
+++ family_expenses/settings/prod.py
@@ -4,9 +4,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['claudio-cuellar.com', 'www.claudio-cuellar.com']
 
-# Security settings for production
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
+CORS_ALLOWED_ORIGINS = [
+    "https://claudio-cuellar.com",
+    "https://www.claudio-cuellar.com",
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 FORCE_SCRIPT_NAME = "/api"
