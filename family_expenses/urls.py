7a552c02b7a359818d115e177e7e9b2a0d69af50
Claudio Cuellar <ccuellar@wine.com>
2025-09-07 07:31:26 -0400

    Updating route

--- family_expenses/urls.py
+++ family_expenses/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-router = DefaultRouter() if settings.DEBUG else SimpleRouter()
+# router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('users/', include('apps.users.urls')),
     path('contacts/', include('apps.contacts.urls')),
 ]
